36eb62232a2527d603f9b9f61e747e93c7344d3c
pedroboga <pedroboga@gmail.com>
2021-08-06 18:51:34 -0300

    Criado esqueleto do código

--- UmaCalculadora/UmaCalculadora/ViewController.swift
+++ UmaCalculadora/UmaCalculadora/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var total = 0
+    var operacao = ""
+    
+    @IBOutlet weak var labelResultado: UILabel!
+    @IBAction func btnCalc(_ sender: UIButton) {
+        if let selecionado = sender.currentTitle {
+            switch selecionado {
+            case "+", "-", "x", "/":
+                if operacao == ""{
+                    self.total = Int(labelResultado.text!)!
+                } else {
+                    performCalc()
+                }
+                operacao = selecionado
+            default:
+                if labelResultado.text == "0" {
+                    labelResultado.text = ""
+                }
+                labelResultado.text! += selecionado
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func performCalc(){
+        
+    }
+    
 }
 
